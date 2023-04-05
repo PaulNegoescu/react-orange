19d3669fa231393b7b35b30e68fb1f07f3957d5d
Paul Negoescu <paul.negoescu@scoalainformala.ro>
2023-04-05 12:10:58 +0300

    Finished with state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,5 +2,7 @@ import React from 'react';
 import { Counter } from '../../features/Counter/Counter';
 
 export function App() {
-  return <Counter />;
+  return <Counter initialCount={3} initialStep={2} />;
 }
+
+// React.createElement(Counter, {initialCount: '3', initalStep: '2'}, null);
--- src/features/Counter/Counter.jsx
+++ src/features/Counter/Counter.jsx
@@ -1,17 +1,73 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
+import clsx from 'clsx';
 
-export function Counter() {
-  const [count, setCount] = useState(0);
+import styles from './Counter.module.css';
+
+function countReducer(oldState, action) {
+  let newState = oldState;
+
+  switch (action.type) {
+    case 'increment':
+      newState += action.payload;
+      break;
+    case 'decrement':
+      newState -= action.payload;
+      break;
+    case 'reset':
+      newState = action.payload;
+      break;
+    default:
+      console.warn(
+        `The action type "${action.type}" is not defined in counterReducer.`
+      );
+  }
+
+  return newState;
+}
+
+export function Counter({ initialCount = 0, initialStep = 1 }) {
+  const [count, dispatch] = useReducer(countReducer, initialCount);
+  const [step, setStep] = useState(initialStep);
+
+  function handleInputChange(e) {
+    setStep(e.target.valueAsNumber);
+  }
 
   return (
     <>
       <h1>Counter</h1>
       <p>
-        <output>{count}</output>
+        <output
+          className={clsx(styles.output, {
+            [styles.positive]: count > 0,
+            [styles.negative]: count < 0,
+          })}
+        >
+          {count}
+        </output>
       </p>
+      <form>
+        <label htmlFor="step">Step</label>{' '}
+        <input
+          type="number"
+          name="step"
+          id="step"
+          value={step}
+          onChange={handleInputChange}
+        />
+      </form>
       <p>
-        <button onClick={() => setCount((oldState) => oldState - 1)}>-</button>
-        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => dispatch({ type: 'decrement', payload: step })}>
+          -
+        </button>
+        <button
+          onClick={() => dispatch({ type: 'reset', payload: initialCount })}
+        >
+          Reset
+        </button>
+        <button onClick={() => dispatch({ type: 'increment', payload: step })}>
+          +
+        </button>
       </p>
     </>
   );
